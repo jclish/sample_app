324ab47aae1b026127a61fa6a9080539f1480a73
jclish <jourdan@clish.com>
2014-09-11 20:38:56 -0700

    Add user following, JC UI changes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,7 +7,7 @@ describe "Static pages" do
   describe "Home page" do
     before { visit root_path }
 
-    it { should have_content('Sample App') }
+    it { should have_content('Jourdan') }
     it { should have_title(full_title('')) }
     it { should_not have_title('| Home') }
     describe "for signed-in users" do
@@ -23,6 +23,16 @@ describe "Static pages" do
         user.feed.each do |item|
           expect(page).to have_selector("li##{item.id}", text: item.content)
         end
+      end
+      describe "follower/following counts" do
+        let(:other_user) { FactoryGirl.create(:user) }
+        before do
+          other_user.follow!(user)
+          visit root_path
+        end
+
+        it { should have_link("0 following", href: following_user_path(user)) }
+        it { should have_link("1 followers", href: followers_user_path(user)) }
       end
     end
   end
